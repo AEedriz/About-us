a4c463103f83089c6c47718339ab6c28d77c24f8
AEedriz <idrisolalekan17@gmail.com>
2022-08-11 07:11:25 -0700

    initial commit

--- script.js
+++ script.js
@@ -9,17 +9,55 @@ $(document).ready(function () {
     });
     // slide-up
    $('.scroll-up-btn').click(function () {
-       $('html').animate({ scrollTop: 0 });
+       $('body').animate({ scrollTop: 0 });
        // removing smooth scroll on slide-up button click
-       $('html').css("scrollBehavior", "auto");
+       $('body').css("scrollBehavior", "auto");
+       console.log('maybe');
    });
    $('.navbar .nav-links li a').click(function () {
        // applying again smooth scroll on menu items click
-       $('html').css("scrollBehavior", "smooth");
+       $('body').css("scrollBehavior", "smooth");
    });
    // toggle menu/navbar
    $('.menu-btn').click(function () {
        $('.navbar .nav-links').toggleClass("active");
        $('.menu-btn i').toggleClass("active");
+       console.log('yes');
    });
 });
+
+const slides = document.querySelectorAll('.profile');
+const nextBtn = document.querySelector('.nextslide')
+const slider = function () {
+    let curSlide = 0;
+    const maxSlide = slides.length - 1;
+  
+    //Functions
+  
+    const goToSlide = function (slide) {
+      slides.forEach((s, i) => {
+        if(i === slide){
+            s.classList.add('show')
+        } else{
+            s.classList.remove('show')
+        }
+      });
+    };
+  
+    //Next slide
+    const nextSlide = function () {
+      if (curSlide === maxSlide) {
+        curSlide = 0;
+      } else {
+        curSlide++;
+      }
+      goToSlide(curSlide);
+    };
+    const init = function () {
+      goToSlide(0);
+    };
+    init();
+    ///Event handlers
+    nextBtn.addEventListener('click', nextSlide);
+}
+slider();
